9fab9612346227267b83c8f2adc7c1dd3ea15ff9
Sri Harsha <sriwithsweetsmile@gmail.com>
2016-11-17 22:37:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
 console.log('Loaded!');
+	function validateForm() 
+		{
+			var x = document.ins.rno.value;
+			if (x == null || x == "") 
+			{
+				alert("Please enter a roll numsber.");
+				document.ins.rno.focus() ;
+				return false;
+			}	
+		}
+    function validateFormi() {
+    var x = document.ext.rnog.value;
+    if (x == null || x == "") {
+        alert("Please enter a roll number.");
+        document.ext.rnog.focus() ;
+        return false;
+    }
+    }
